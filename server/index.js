59e06b7fd5c7beb6736be4dddc0fb647ca520275
lokeshk2492 <lokeshk2492@gmail.com>
2026-02-27 18:18:32 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -167,7 +167,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(distPath));
 
     // SPA fallback - serve index.html for all non-API routes
-    app.get('*', (req, res) => {
+    app.get((req, res) => {
         if (!req.path.startsWith('/api')) {
             res.sendFile(path.join(distPath, 'index.html'));
         }
