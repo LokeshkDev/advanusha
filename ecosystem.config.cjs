f083c2af9bd0e8cb1137d904b197244c9370841b
lokeshk2492 <lokeshk2492@gmail.com>
2026-02-27 17:12:21 +0530

    add production server

---
+++ ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+        {
+            name: 'advanusha',
+            script: 'server/index.js',
+            env: {
+                NODE_ENV: 'production',
+                PORT: 5000
+            },
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '500M'
+        }
+    ]
+};
